029ec2e2759e5ff7b351fa49d8afd8098185e049
Alex Dollison <alexanderdollison@gmail.com>
2024-09-22 13:05:33 -0500

    Module 3 completed baseline

--- app_server/routes/travel.js
+++ app_server/routes/travel.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var router = express.Router();
-var controller = require('../controllers/travel');
+var controller = require('../controllers/travel').default;
 
 /* GET travel page. */
 router.get('/', controller.travel);
